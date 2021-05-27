fa0406aa6ee8c0751414565bd5da7fa05b5e98e4
Chetan <61468014+Unknown39825@users.noreply.github.com>
2021-05-27 09:43:50 +0530

    inital setup

---
+++ README.md
@@ -0,0 +1,30 @@
+# FineArts-BackEnd
+FineArts 
+
+
+## Project setup
+```
+npm install
+```
+
+### run test command
+```
+npm test
+```
+
+### To Start Testing Frontend
+
+
+```
+cd TestFront
+```
+
+
+```
+npm i
+```
+
+```
+npm starts
+```
+
---
+++ index.js
@@ -0,0 +1,45 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const passport = require('passport');
+
+// my routes
+
+//DB Connection
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log("DB CONNECTED");
+  });
+
+const app = express();
+app.use(cors());
+
+//Middlewares
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(express.urlencoded({ extended: false }));
+mongoose.Promise = global.Promise;
+
+app.use(passport.initialize());
+
+//My Routes
+
+app.use("/",(req,res)=>{
+  res.send("Welcome to FreeOSenior");
+})
+
+const port = process.env.PORT || 8000;
+
+//Starting a server
+app.listen(port, () => {
+  console.log(`app is running at ${port}`);
+});
